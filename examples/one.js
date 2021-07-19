61b11b7e66b4d5eadc8980932337489ebd6963e1
Eyal Arasu <elarasu@gmail.com>
2021-07-19 16:39:05 -0700

    feat: service class updated to have get method

--- examples/one.js
+++ examples/one.js
@@ -1,6 +1,23 @@
-import { double, getStatus } from '../build/module/lib/number.js';
+// import { double, getStatus } from '../build/module/lib/number.js';
 import { Service } from '../build/module/lib/service.js';
 
-console.log(double(3));
-getStatus('localhost:3000', 'test', 'test123');
-console.log(new Service('localhost:3000'));
+// console.log(double(3));
+// getStatus('localhost:3000', 'test', 'test123');
+// console.log(new Service('localhost:3000'));
+const translate = new Service('https://inputtools.google.com');
+translate.setParams({
+  itc: 'ta-t-i0-und',
+  num: 13,
+  ie: 'utf-8',
+  oe: 'utf-8',
+});
+// text\=welcome\&itc\=ta-t-i0-und\&num\=13\&ie\=utf-8\&oe\=utf-8
+translate.get('/request', {
+  text: 'welcome',
+});
+translate.get('/request', {
+  text: 'hello',
+});
+translate.get('/request', {
+  text: 'world',
+});
--- src/lib/service.ts
+++ src/lib/service.ts
@@ -1,7 +1,35 @@
+import { create, ApisauceInstance } from 'apisauce';
+import RS from 'ramdasauce';
 export class Service {
   _url: string;
+  _api: ApisauceInstance;
+  _params: object;
+
   constructor(url: string) {
     this._url = url;
     console.log('****** Service URL set to ******', this._url);
+    this._api = create({
+      baseURL: url,
+    });
+    // attach a monitor that fires with each request
+    this._api.addRequestTransform((request) => {
+      console.log('******** request sent *******', request);
+    });
+  }
+
+  setParams(params: object) {
+    this._params = params;
+  }
+
+  get(path: string, params: object) {
+    const p = {
+      ...this._params,
+      ...params,
+    };
+    this._api
+      .get(path, p)
+      .then(RS.dotPath('data'))
+      .then(JSON.stringify)
+      .then(console.log);
   }
 }
