c0fbb5d0df8b7c284c7183c5727a088ee97af8ed
Eyal Arasu <elarasu@gmail.com>
2021-07-22 01:13:29 -0700

    feat(cache): service request with request/response interceptors (cache)

--- examples/one.js
+++ examples/one.js
@@ -1,5 +1,7 @@
 // import { double, getStatus } from '../build/module/lib/number.js';
 import { Service } from '../build/module/lib/service.js';
+import LRU from 'lru-cache';
+import RS from 'ramdasauce';
 
 // console.log(double(3));
 // getStatus('localhost:3000', 'test', 'test123');
@@ -11,13 +13,67 @@ translate.setParams({
   ie: 'utf-8',
   oe: 'utf-8',
 });
+translate.setCache(200);
 // text\=welcome\&itc\=ta-t-i0-und\&num\=13\&ie\=utf-8\&oe\=utf-8
-translate.get('/request', {
-  text: 'welcome',
+// translate.get('/request', {
+//   text: 'welcome',
+// });
+// translate.api
+//   .get('/request', {
+//     text: 'welcome',
+//   })
+//   .then(RS.dotPath('data'))
+//   .then(console.log);
+translate
+  .get('/request', {
+    text: 'hello',
+  })
+  .then(RS.dotPath('data'))
+  .then((response) => {
+    console.log(response);
+    translate
+      .get('/request', {
+        text: 'world',
+      })
+      .then(RS.dotPath('data'))
+      .then(console.log);
+    translate
+      .get('/request', {
+        text: 'hello',
+      })
+      .then(RS.dotPath('data'))
+      .then(console.log);
+  });
+// translate.get('/request', {
+//   text: 'world',
+// });
+
+var cache = new LRU({
+  max: 3,
+  lengthit: function (n, key) {
+    console.log('***** length ****', n, key);
+    return n * 2 + key.length;
+  },
+  dispose: function (key, n) {
+    console.log('======= dispose ==== ');
+    console.log(key);
+    console.log(n);
+    // n.close();
+  },
+  maxAgeit: 1000 * 60 * 60,
 });
-translate.get('/request', {
-  text: 'hello',
-});
-translate.get('/request', {
-  text: 'world',
-});
+cache.set('key', 'value');
+console.log(cache.get('key'));
+cache.set('key1', 'value1');
+console.log(cache.get('key1'));
+console.log(cache.dump());
+cache.set('key2', 'value2');
+console.log(cache.get('key2'));
+cache.set('key3', 'value3');
+console.log(cache.get('key3'));
+console.log(cache.dump());
+cache.set('key4', 'value4');
+console.log(cache.get('key4'));
+cache.set('key5', 'value5');
+console.log(cache.get('key5'));
+console.log(cache.dump());
--- src/lib/service.ts
+++ src/lib/service.ts
@@ -1,35 +1,91 @@
-import { create, ApisauceInstance } from 'apisauce';
-import RS from 'ramdasauce';
+import { create, ApisauceInstance, HEADERS } from 'apisauce';
+import LRU from 'lru-cache';
+
+// import RS from 'ramdasauce';
 export class Service {
   _url: string;
   _api: ApisauceInstance;
-  _params: object;
+  _params: object = null;
+  _cache: LRU = null;
 
-  constructor(url: string) {
+  constructor(url: string, headers: HEADERS = null) {
     this._url = url;
-    console.log('****** Service URL set to ******', this._url);
+    // console.log('****** Service URL set to ******', this._url);
     this._api = create({
       baseURL: url,
+      headers,
     });
     // attach a monitor that fires with each request
-    this._api.addRequestTransform((request) => {
-      console.log('******** request sent *******', request);
+    // this._api.addRequestTransform((request) => {
+    //   console.log('******** request sent *******', request);
+    // });
+    // change the get fn
+    const originalGet = this._api.get;
+    this._api.get = (url, params = {}) => {
+      return new Promise((resolve) => {
+        const key = url + JSON.stringify(params);
+        // console.log('***** inside modified get ******');
+        if (this._cache.has(key)) {
+          const val = this._cache.get(key);
+          const data = JSON.parse(val);
+          console.log(`[CACHE GET] ${key} ${val}`);
+          return resolve({
+            ok: true,
+            problem: null,
+            originalError: null,
+            data,
+          });
+        }
+        return resolve(originalGet(url, params));
+      });
+    };
+
+    this._api.addResponseTransform((response) => {
+      // console.log('******** response received *******', response);
+      // lets add the ressult to cache
+      if (this._cache && response.ok) {
+        const key =
+          response.config.url + JSON.stringify(response.config.params);
+        const val = JSON.stringify(response.data);
+        console.log(`[CACHE SAVE] ${key} ${val}`);
+        this._cache.set(key, val);
+      }
     });
+  }
+
+  setCache(limit: number) {
+    this._cache = new LRU({
+      max: limit,
+      dispose: function (key, n) {
+        console.log('======= dispose ==== ');
+        console.log(key);
+        console.log(n);
+        // n.close();
+      },
+      maxAgeit: 1000 * 60 * 60,
+    });
+  }
+
+  setHeaders(headers: HEADERS) {
+    this._api.setHeaders(headers);
   }
 
   setParams(params: object) {
     this._params = params;
   }
 
-  get(path: string, params: object) {
+  get api(): ApisauceInstance {
+    return this._api;
+  }
+
+  get(path: string = '/', params: object = null) {
     const p = {
       ...this._params,
       ...params,
     };
-    this._api
-      .get(path, p)
-      .then(RS.dotPath('data'))
-      .then(JSON.stringify)
-      .then(console.log);
+    return this._api.get(path, p);
+    // .then(RS.dotPath('data'))
+    // .then(JSON.stringify)
+    // .then(console.log);
   }
 }
